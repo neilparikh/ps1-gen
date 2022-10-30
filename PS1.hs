5c6ae6b23691777070c8753aeac13f6a1005fc0a
Neil Parikh <n3parikh@csclub.uwaterloo.ca>
2022-10-30 03:59:30 -0400

    initial commit

---
+++ PS1.hs
@@ -0,0 +1,31 @@
+main = do
+  let prompt = [ColoredText Magenta username,
+                Text " in ",
+                ColoredText Green fullPWD,
+                Text "\\$([[ -n \\$(git branch 2> /dev/null) ]] && echo \\\" on \\\")",
+                ColoredText Purple (Text "\\$(parse_git_branch)"),
+                newline,
+                Text "\\$ "
+                ]
+  header <- readFile "prompt.bash"
+  putStrLn $ header ++ "PS1=\"" ++ concatMap generate prompt ++ "\""
+
+data Color = Magenta | Orange | Green | Purple | White
+
+instance (Show Color) where
+  show Magenta = "MAGENTA"
+  show Orange  = "ORANGE"
+  show Green   = "GREEN"
+  show Purple  = "PURPLE"
+  show White   = "WHITE"
+
+data Entry = Text String
+           | ColoredText Color Entry
+
+generate :: Entry -> String
+generate (Text s) = s
+generate (ColoredText c e) = "\\[$" ++ (show c) ++ "\\]" ++ generate e ++ "\\[$RESET\\]"
+
+username = Text "\\u"
+fullPWD = Text "\\w"
+newline = Text "\\n"
